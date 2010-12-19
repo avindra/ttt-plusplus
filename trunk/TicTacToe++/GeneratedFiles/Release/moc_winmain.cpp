aca1221f394695b8dfa229957035d36f85ea86c0
aavindraa <aavindraa@130b13e9-9e05-cdb3-f61f-84469f77c15d>
2010-12-19 21:22:47 +0000

    Working on connecting buttons to functions. The two exit buttons work, nothing else though.

--- trunk/TicTacToe++/GeneratedFiles/Release/moc_winmain.cpp
+++ trunk/TicTacToe++/GeneratedFiles/Release/moc_winmain.cpp
@@ -1,7 +1,7 @@
 /****************************************************************************
 ** Meta object code from reading C++ file 'winmain.h'
 **
-** Created: Sun Dec 19 01:04:21 2010
+** Created: Sun Dec 19 16:21:13 2010
 **      by: The Qt Meta Object Compiler version 62 (Qt 4.7.1)
 **
 ** WARNING! All changes made in this file will be lost!
--- trunk/TicTacToe++/winmain.cpp
+++ trunk/TicTacToe++/winmain.cpp
@@ -1,17 +1,47 @@
 #include "winmain.h"
+#include <QMessageBox>
 
 winMain::winMain(QWidget *parent, Qt::WFlags flags)
 	: QMainWindow(parent, flags)
 {
 	ui.setupUi(this);
+
+	// Find the main "grdGame" as "grid"
 	QGridLayout * grid = findChild<QGridLayout *>("grdGame");
+
+	// Connect the exit button in the main menu to the quit function.
+	QAction * mnuExit = findChild<QAction*>("mnuExit");
+	connect(mnuExit, SIGNAL(triggered(bool)), qApp, SLOT(quit()));
+
+
+	// Connect the about button in the help menu to the function
+	QAction * about = findChild<QAction*>("mnuAbout");
+	connect(about, SIGNAL(triggered(bool)), qApp, SLOT(aboutGame()));
+
+
+	// Connect the exit button in the main form to the quit function.
+	QPushButton * exit = findChild<QPushButton*>("btnExit");
+	connect(exit, SIGNAL(clicked()), qApp, SLOT(quit()));
+
+	// Set up the board
 	for(int r = 0; r < 3; ++r) {
 		for(int c = 0; c < 3; ++c) {
 			QPushButton  * btn = new QPushButton();
+
+			// Set button to fill it's allotted space
 			btn->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Expanding);
+
+			// Connect the button to the event
+		//	connect(btn, SIGNAL(clicked()), qApp, SLOT(winMain::btnClicked()));
+
+			// Add it to the board
 			grid->addWidget(btn, r, c);
 		}
 	}
+}
+
+void winMain::aboutGame() {
+	QMessageBox::about(this, "About this game", "<h1>Tic-Tac-Toe++</h1>");
 }
 
 winMain::~winMain()
