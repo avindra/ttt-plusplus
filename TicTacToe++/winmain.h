cd50471309798218980f0357c8b6a9b6a326b731
Avindra Goolcharan <aavindraa@gmail.com>
2010-12-20 05:39:13 +0000

    -Little Board.cpp work -Break out of setter functions if the buttons are flat -Primitive X/O set up -Set up dummy new game for new game button

--- TicTacToe++/winmain.h
+++ TicTacToe++/winmain.h
@@ -9,10 +9,14 @@ class winMain : public QMainWindow
 {
 	Q_OBJECT
 	public:
+
+		bool xHasTurn;
+
 		// Construct / destruct
 		winMain(QWidget *parent = 0, Qt::WFlags flags = 0);
 		~winMain();
 	public slots:
+		void newGame();
 		void aboutGame();
 		void btnPressed();
 
