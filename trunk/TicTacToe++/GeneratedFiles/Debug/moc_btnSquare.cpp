cc630a7dc141692ff5c0dfe2ad93a3f4207117d7
aavindraa <aavindraa@130b13e9-9e05-cdb3-f61f-84469f77c15d>
2011-01-27 15:20:01 +0000

    Define DEBUG in preprocessor for Debug releases.

--- trunk/TicTacToe++/GeneratedFiles/Debug/moc_btnSquare.cpp
+++ trunk/TicTacToe++/GeneratedFiles/Debug/moc_btnSquare.cpp
@@ -1,7 +1,7 @@
 /****************************************************************************
 ** Meta object code from reading C++ file 'btnSquare.h'
 **
-** Created: Fri Dec 24 17:00:10 2010
+** Created: Thu Jan 27 10:18:08 2011
 **      by: The Qt Meta Object Compiler version 62 (Qt 4.7.1)
 **
 ** WARNING! All changes made in this file will be lost!
